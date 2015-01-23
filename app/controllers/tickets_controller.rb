84955f32c7a788e28bde520b71c6a6ee7c2cae01
Jim Nutt <jim@nuttz.org>
2015-01-23 12:21:56 -0500

    Added save and show functionaliyy to the tickets controller Added show view Added link to new in index, but doesn't actually work

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,10 +1,24 @@
 class TicketsController < ApplicationController
 
+  def index
+  end
+
+  def show
+    @ticket = Ticket.find(params[:id])
+  end
+  
   def new
   end
 
   def create
-    render plain: params[:ticket].inspect
+    @ticket = Ticket.new(ticket_params)
+    @ticket.save
+    redirect_to @ticket
+  end
+
+  private
+  def ticket_params
+    params.require(:ticket).permit(:issue, :client_email, :submit_date, :description)
   end
   
 end
